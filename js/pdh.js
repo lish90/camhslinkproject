6e7063c6a0ebcfa5f0b096f1792d108674b9971b
lish <34666426+lish90@users.noreply.github.com>
2021-12-30 21:29:38 +0000

    startingJS

---
+++ js/pdh.js
@@ -0,0 +1,21 @@
+// current function saved in the html as dummy before 
+// creating function that score the users questionnaire:"myFunction();
+
+
+//  possible function 1 
+
+function pdhFrequency(params) {
+    scoreFre = []
+
+    if (value === "1")
+        scoreFre =+ 1
+    
+    else 
+    
+}
+
+
+// possible function 2 
+function name(params) {
+    
+}
